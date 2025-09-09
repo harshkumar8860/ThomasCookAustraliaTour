93982151fefeeb1b16759d11edd17543f64a71fd
harshKumar8860 <harshkumar8860@gmail.com>
2025-09-09 06:51:36 +0530

    added extent reports

--- src/test/java/testRunners/RunCucumberTest.java
+++ src/test/java/testRunners/RunCucumberTest.java
@@ -4,14 +4,22 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = "src/test/resources/features/",
-    glue = {"stepDefinitions","hooks"},
-    plugin = {"pretty","html:target/cucumber-reports.html"},
+		features = "src/test/resources/features",
+	    glue = {"stepDefinitions", "hooks"},
+	    plugin = {
+	        "pretty",
+	        "html:target/cucumber-reports/cucumber.html",
+	        "json:target/cucumber-reports/cucumber.json",
+	        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+	    },
     monochrome = true
 )
 
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 }
+
+
+
 
 
 
